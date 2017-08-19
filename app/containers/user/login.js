09a38157a1ad36079d51c7c510bbac6fc2c7d6e3
txasela <asela.wijesinghe@taxien.com>
2017-08-19 17:27:30 +0530

    Testing UI component import

--- app/containers/user/login.js
+++ app/containers/user/login.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { BackgroundColorTXHome, PrimaryColorTXHome } from 'TXRNThemeManager';
 import {
   View,
   Text
 } from 'react-native';
-import { BackgroundColorTXHome, PrimaryColorTXHome } from 'TXRNThemeManager';
 
 const Container = styled.View`
   background-color: ${props => props.bgcolor};
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   flex: 1;
+`;
+
+const Header = styled.View`
+  background-color: ${props => props.bgcolor};
+  display: flex;
+  flex-direction: column;
+  flex: 0.08;
+`;
+
+const CenteredTitle = styled.Text`
+  align-self: center;
+  font-size: 18px;
+  color:  ${props => props.color};
+  margin: 20px;
 `;
 
 const Login = () => (
   <Container
-    bgcolor={PrimaryColorTXHome}
+    bgcolor={BackgroundColorTXHome}
   >
-    <Container
-      bgcolor={BackgroundColorTXHome}
+    <Header
+      bgcolor={PrimaryColorTXHome}
     >
-      <Text >LOGIN</Text>
-    </Container>
+      <CenteredTitle
+        color={BackgroundColorTXHome}
+      >SPAKL
+      </CenteredTitle>
+    </Header>
+
   </Container>
 );
 
